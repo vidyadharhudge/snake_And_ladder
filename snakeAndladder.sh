09d3acdb7cf7fe892300786353dbcccdc3ca58d0
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-23 18:25:40 +0530

    Added:-player reaches winning poistion 100

--- snakeAndladder.sh
+++ snakeAndladder.sh
@@ -9,30 +9,32 @@ END_POISTION=100
 current_Poistion=0
 
 echo "player rolls the die and get the numbers"
+while [[ $current_Poistion -lt $END_POISTION ]]
+do
 diceRoll=$((RANDOM%6 +1))
-
 option=$((RANDOM%3 +1))
-case $option in
 
-1)
-     echo "no play"
-     current_Poistion=$current_Poistion
-     ;;
+   case $option in
 
-2)
-     echo "ladder"
-     current_Poistion=$((current_Poistion+diceRoll))
-     ;;
+   1)
+       echo "no play"
+       current_Poistion=$current_Poistion
+       ;;
 
-3)
-     echo "snake"
-     if [[ $diceRoll -gt $current_Poistion ]]
-     then
-     current_Poistion=$current_Poistion
-     else
-     current_Poistion=$current_Poistion-diceRoll
-     fi
-     ;;
+   2)
+       echo "ladder"
+       current_Poistion=$((current_Poistion+diceRoll))
+       ;;
 
-esac
+   3)
+       echo "snake"
+       if [[ $diceRoll -gt $current_Poistion ]]
+       then
+       current_Poistion=$current_Poistion
+       else
+       current_Poistion=$((current_Poistion-diceRoll))
+       fi
+       ;;
 
+   esac
+done
