b44a63c2ef4a6d0320ae3c3f1ff3f522845e7d96
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-24 12:03:57 +0530

    Added:-win between two player

--- snakeAndladder.sh
+++ snakeAndladder.sh
@@ -1,53 +1,64 @@
-#!/bin/bash -x  
+#!/bin/bash -x
 echo "welcome to Snake and ladder game"
+dicePlayerOne=0
+dicePlayerTwo=0 
+function twoPlayer()
+{ 
 
-#CONSTANT
-START_POISTION=0
-END_POISTION=100
+   START_POISTION=0
+   END_POISTION=100
+   current_Poistion=0
+   no_Of_die=0
+   while [[ $current_Poistion -lt $END_POISTION ]]
+   do
+      diceRoll=$((RANDOM%6 +1))
+      option=$((RANDOM%3 +1))
+   	noPlay=1
+	   ladder=2
+	   snake=3
+      case $option in
 
-#VARIBALE
-current_Poistion=0
-no_Of_die=0
-echo "player rolls the die and get the numbers"
-while [[ $current_Poistion -lt $END_POISTION ]]
-do
-diceRoll=$((RANDOM%6 +1))
-option=$((RANDOM%3 +1))
+      $noPlay)
+              current_Poistion=$current_Poistion
 
-   case $option in
+              ;;
 
-   1)
-       echo "no play"
-       current_Poistion=$current_Poistion
-       ;;
+      $ladder)
+              Current_Poistion=$((current_Poistion+diceRoll))
+              ((no_Of_die++))
+              if [[ $Current_Poistion -gt $END_POISTION ]]
+              then
+                    current_Poistion=$current_Poistion
+              else
+                    current_Poistion=$Current_Poistion
+              fi 
+ 
+              ;;
 
-   2)
-       echo "ladder"
-       Current_Poistion=$((current_Poistion+diceRoll))
-       no_Of_die=$((no_Of_die+1))
-       if [[ $Current_Poistion -gt $END_POISTION ]]
-       then
-            current_Poistion=$current_Poistion
-       else
-            current_Poistion=$Current_Poistion
-       fi 
-        array_Dice[$no_Of_die]=$current_Poistion
-        echo "${!array_Dice[@]}"
-        echo " ${array_Dice[@]}"
-        echo "ladder is:$current_Poistion"
-       ;;
+      $snake)
+             Current_Poistion=$((current_Poistion-diceRoll))
+            if [[ $Current_Poistion -lt $START_POISTION ]]
+            then
+                 current_Poistion=$current_Poistion
+            else
+                 current_Poistion=$Current_Poistion
+            fi
 
-   3)
-       echo "snake"
-       Current_Poistion=$((current_Poistion-diceRoll))
-       if [[ $Current_Poistion -lt $START_POISTION ]]
-       then
-       current_Poistion=$current_Poistion
-       else
-       current_Poistion=$Current_Poistion
-       fi
-           echo "snake is:$current_Poistion"
-       ;;
+            ;;
 
-   esac
-done
+       esac
+
+   done
+   echo $no_Of_die
+}
+
+playerOne="$( twoPlayer $(()) )"
+playerTwo="$( twoPlayer $(()) )"
+echo "playerone=$playerOne"
+echo "playertwo=$playerTwo"
+if [[ $playerOne -lt $playerTwo ]]
+then
+     echo "playerOne is win"
+else
+     echo "playerTwo is win"
+fi
