25d56c5d17ed636d1d3047e6cfe993e227dd9243
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-07 21:34:26 +0530

    Added:-Play game with two player

--- snakeAndladder.sh
+++ snakeAndladder.sh
@@ -1,64 +1,79 @@
-#!/bin/bash -x
-echo "welcome to Snake and ladder game"
-dicePlayerOne=0
-dicePlayerTwo=0 
-function twoPlayer()
-{ 
-
-   START_POISTION=0
-   END_POISTION=100
-   current_Poistion=0
-   no_Of_die=0
-   while [[ $current_Poistion -lt $END_POISTION ]]
-   do
-      diceRoll=$((RANDOM%6 +1))
-      option=$((RANDOM%3 +1))
-   	noPlay=1
-	   ladder=2
-	   snake=3
-      case $option in
-
-      $noPlay)
-              current_Poistion=$current_Poistion
-
-              ;;
-
-      $ladder)
-              Current_Poistion=$((current_Poistion+diceRoll))
-              ((no_Of_die++))
-              if [[ $Current_Poistion -gt $END_POISTION ]]
-              then
-                    current_Poistion=$current_Poistion
-              else
-                    current_Poistion=$Current_Poistion
-              fi 
- 
-              ;;
-
-      $snake)
-             Current_Poistion=$((current_Poistion-diceRoll))
-            if [[ $Current_Poistion -lt $START_POISTION ]]
-            then
-                 current_Poistion=$current_Poistion
-            else
-                 current_Poistion=$Current_Poistion
-            fi
-
-            ;;
-
-       esac
-
-   done
-   echo $no_Of_die
+#!/bin/bash 
+echo "welcome to snakeladder" 
+STARTPOSITION=0 
+WINNINGPOSITION=100 
+currentPosition=$STARTPOSITION
+playerOne=$STARTPOSITION
+playerTwo=$STARTPOSITION
+#roll the die for change position
+function rollDie()
+{
+	diceRoll=$((RANDOM%6 +1)) 
+	checkOption
+}
+#chack option for snake ladder or no position
+function checkOption()
+{
+	NOPLAY=0
+	SNAKE=1
+	LADDER=2
+	option=$((RANDOM%3))
+	case $option in
+		$NOPLAY)
+			currentPosition=$currentPosition
+			;;
+		$SNAKE)	
+			((currentPosition-=$diceRoll))
+			if [ $currentPosition -lt $STARTPOSITION ]
+			then
+				currentPosition=$STARTPOSITION
+			fi
+			;;
+		$LADDER)
+			((currentPosition+=$diceRoll))
+			((numberOfTimeDieThrowForWin++))
+			if [ $currentPosition -gt $WINNINGPOSITION ]
+			then
+				((currentPosition-=$diceRoll))
+			fi
+			;;
+	esac
+}
+#update position
+function updatePosition()
+{
+	while [ $currentPosition -lt $WINNINGPOSITION ]
+	do
+   	rollDie
+	done
+}
+#player one play the game
+function playerOne()
+{
+	numberOfTimeDieThrowForWin=0
+	updatePosition
+	playerOneCount=$numberOfTimeDieThrowForWin
+	echo $playerOneCount
+}
+#player two play the game
+function playerTwo()
+{
+	STARTPOSITION=0 
+	WINNINGPOSITION=100 
+	currentPosition=$STARTPOSITION
+	numberOfTimeDieThrowForWin=0
+	updatePosition
+	playerTwoCount=$numberOfTimeDieThrowForWin
+	echo $playerTwoCount
 }
 
-playerOne="$( twoPlayer $(()) )"
-playerTwo="$( twoPlayer $(()) )"
-echo "playerone=$playerOne"
-echo "playertwo=$playerTwo"
-if [[ $playerOne -lt $playerTwo ]]
+result1="$( playerOne $(()) )"
+result2="$( playerTwo $(()) )"
+echo "player one : $result1"
+echo "player two : $result2"
+if [ $result1 -lt $result2 ]
 then
-     echo "playerOne is win"
+   echo "player one win"
 else
-     echo "playerTwo is win"
+   echo "player two win"
 fi
